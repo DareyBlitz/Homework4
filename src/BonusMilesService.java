85cff5583e91a509a29127146115783642df7eda
Darey Blitz <dareyblitz@vk.com>
2023-04-17 18:41:28 +0300

    correct BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,7 @@
 public class BonusMilesService {
 
     public int calculate(int price) {
-        //int miles = 20;
         System.out.println(price / 20);
-        //return miles;
         return price;
     }
 }
