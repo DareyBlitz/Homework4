aee2a90a0ef10397cdbb7512509e22a364c13faf
Darey Blitz <dareyblitz@vk.com>
2023-04-17 18:38:05 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        //int miles = 20;
+        System.out.println(price / 20);
+        //return miles;
+        return price;
+    }
+}
